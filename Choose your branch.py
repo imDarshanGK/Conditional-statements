c3815e783c6348a081d2523fa202f8a87a0a74d1
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-01-26 22:52:32 +0530

    Conditional-statement
    if elif ladder

---
+++ Choose your branch.py
@@ -0,0 +1,15 @@
+print("Welcome to SIET College")
+print('1}ECE\n2}EEE\n3}CSE\n4}ISE\n5}AI&DS')
+c=int(input('Choose your branch:'))
+if c==1:
+    print('You selected ECE')
+elif c==2:
+    print('You selected EEE')
+elif c==3:
+    print('You selected CSE')
+elif c==4:
+    print('You selected ISE')
+elif c==5:
+    print('You selected AI&DS')
+else:
+    print('Invalid choice')
