2684cde94ee5b30d4b79ef1715cbbee281d6712e
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-01-15 18:12:01 +0530

    if statement

---
+++ If statement.py
@@ -0,0 +1,6 @@
+#If statement
+age=int(input("Enter your age:"))
+print('Your age is',age)
+if(age>18):
+    print("Eligible to vote")
+print("User")
---
+++ If.py
@@ -0,0 +1,7 @@
+#If Statement
+a=9
+b=10
+if a>b:
+    print("a is grater than b")
+print("user")
+print("password")
